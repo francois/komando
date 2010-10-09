55c00c51dd04d450a6b74799face398f236bd741
François Beausoleil <francois@teksol.info>
2010-10-09 13:44:35 -0400

    Persistence docs

---
+++ lib/komando/persistence.rb
@@ -0,0 +1,6 @@
+module Komando
+  # Persistence helpers that wrap {Komando::Command} executions within
+  # transactions, or other mechanisms that ease use with persistence.
+  module Persistence
+  end
+end
--- lib/komando/persistence/active_record.rb
+++ lib/komando/persistence/active_record.rb
@@ -3,18 +3,52 @@ require "active_record"
 
 module Komando
   module Persistence
+    # Wraps command executions within a database transaction.
     module ActiveRecord
 
+      # Insinuates this module within {Komando::Command}.
+      #
+      # @private
       def self.included(base)
         base.send :alias_method_chain, :run!, :transaction
       end
 
+      # Wraps a command execution within a database transactions.
+      # This method delegates actual transaction semantics to {#wrap_transaction}.
+      # This method is renamed as {Komando::Command##run!} during inclusion.
       def run_with_transaction!
         wrap_transaction do
           run_without_transaction!
         end
       end
 
+      # Does the actual work of wrapping in a transaction. The default is to wrap
+      # using {ActiveRecord::Base#transaction}.
+      #
+      # @example Wrapping using a specific connection
+      #   # config/database.yml
+      #   development:
+      #     adapter: sqlite3
+      #     database: db/development.sqlite3
+      #
+      #   require "komando/persistence/active_record"
+      #
+      #   class User < ActiveRecord::Base
+      #     establish_connection :adapter => "sqlite3", :database => "/var/dbs/users.sqlite3"
+      #   end
+      #
+      #   class CreateUserCommand
+      #     include Komando::Command
+      #     include Komando::Persistence::ActiveRecord
+      #
+      #     def wrap_transaction
+      #       User.transaction do
+      #         yield
+      #       end
+      #     end
+      #   end
+      #
+      # @yieldreturn The block's last value.
       def wrap_transaction
         ::ActiveRecord::Base.transaction do
           yield
